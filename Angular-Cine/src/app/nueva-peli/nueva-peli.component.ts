cd0c040980034f0861f3fc5ad80e228cf9c1aa23
Kirissuno <arfilip.1h@gmail.com>
2019-12-10 13:32:03 +0100

    Fix de error sin login
    :)

--- Angular-Cine/src/app/nueva-peli/nueva-peli.component.ts
+++ Angular-Cine/src/app/nueva-peli/nueva-peli.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pelicula } from '../pelicula';
 import { PeliculaService } from '../pelicula.service';
 import { Router } from '@angular/router';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-nueva-peli',
@@ -11,11 +12,14 @@ import { Router } from '@angular/router';
 export class NuevaPeliComponent implements OnInit {
   pelicula:Pelicula;
 
-  constructor(private peliService:PeliculaService, private router: Router) {
+  constructor(private adminService:AdminService, private peliService:PeliculaService, private router: Router) {
     this.pelicula = new Pelicula();
    }
 
   ngOnInit() {
+    if(this.adminService.logeado === false){
+      this.router.navigate(['filmografia']);
+    }
   }
 
   save(){
